462b89ba411ca7ebdc1fa13c0dc616bf638f3e68
lycm <lycmsky@qq.com>
2022-02-27 00:25:14 +0800

    优化代码规范性，添加注释

--- modules/ImageClassify/__init__.py
+++ modules/ImageClassify/__init__.py
@@ -1,9 +1,4 @@
-from distutils.log import info
-import re
-from urllib import response
-from aip import AipImageClassify
 import json, re
-import asyncio
 from graia.broadcast.interrupt.waiter import Waiter
 from graia.ariadne.message.parser.base import MatchContent
 from graia.ariadne import get_running
@@ -15,39 +10,45 @@ from graia.ariadne.model import Group, Member
 from graia.broadcast.interrupt import InterruptControl
 from graia.saya import Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
+from distutils.log import info
+from aip import AipImageClassify
 
-# 获取插件实例
+# 获取插件实例，添加插件信息
 channel = Channel.current()
 channel.name("识图")
 channel.description("通过百度api识别图片")
 channel.author("Lycm")
 
-# 读取配置文件
+# 读取配置文件，获取百度开放平台api创建实例
 with open("./modules/ImageClassify/config.json",'r') as load_f:
     datainfo = json.load(load_f)
 client = AipImageClassify(datainfo['APP_ID'], datainfo['API_KEY'], datainfo['SECRET_KEY'])
 
+# 创建消息处理器，接收群消息中的 .识图 命令
 @channel.use(ListenerSchema(
     listening_events=[GroupMessage], 
     decorators=[MatchContent(".识图")]
 ))
-async def friend_message_listener(app: Ariadne, group: Group, member: Member, message: MessageChain):
-    inc = InterruptControl(app.broadcast)
+async def imageClassify(app: Ariadne, group: Group, member: Member, message: MessageChain):
+    inc = InterruptControl(app.broadcast) # 创建实例
     await app.sendGroupMessage(group, MessageChain.create('请发送图片'))
-
+    # 交互等待函数
     @Waiter.create_using_function([GroupMessage])
-    async def setu_tag_waiter(g: Group, m: Member, msg: MessageChain):
+    async def waiter(g: Group, m: Member, msg: MessageChain):
+        # 判断新消息的群组和用户是否和上文相同，相同则继续。否则结束。
         if group.id == g.id and member.id == m.id:
             return msg
 
-    a = await inc.wait(setu_tag_waiter)
+    # 处理图片消息，获取图片的url
+    imginfo = await inc.wait(waiter)
+    imgurl = re.findall(r'"url":"(.*?)"', imginfo.asPersistentString())[0]
+    # 通过图片url获取图片二进制编码，并传入识图实例
     session = get_running(Adapter).session
-    imgurl = re.findall(r'"url":"(.*?)"', a.asPersistentString())[0]
     async with session.get(imgurl) as r:
-        data = await r.read()
-        send = client.advancedGeneral(data)['result'][0]
-        response = f'''图片识别结果:
-        关键词：{send['keyword']}
-        准确度：{send['score']*100}%
-        标  签：{send['root']}'''
-    await app.sendGroupMessage(group, MessageChain.create(response))
+        imgBase = await r.read()
+        info = client.advancedGeneral(imgBase)['result'][0]
+        responseMsg = f'''图片识别结果:
+        关键词：{info['keyword']}
+        准确度：{info['score']*100}%
+        标  签：{info['root']}'''
+    await app.sendGroupMessage(group, MessageChain.create(responseMsg))
