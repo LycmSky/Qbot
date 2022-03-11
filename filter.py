0190e39dc8f7b49596e035578c94a7aac0f55f3c
lycm <lycmsky@qq.com>
2022-03-11 22:39:55 +0800

    重构识图功能，增加筛选模块

---
+++ filter.py
@@ -0,0 +1,73 @@
+from turtle import pen
+import database
+
+mods = database.databaseInit('mods')
+
+class Filter:
+    def __init__(self, mod_name=False, event=False):
+        '''进行条件判断：
+            mod_name --> mod的名字
+            event --> 消息的event对象
+        '''
+        self.mod_name = mod_name
+        self.event = event
+    
+    def main(self, code: int):
+        '''主函数，调用其他函数：
+            cede => int 解析为二进制（如0100011)，控制启用哪些判断
+            #1 enable --> 确认模块是否开启
+            #2 state --> 确认用户是否开启功能
+            #3 blacklist --> 确认用户是否被拉黑
+            #4 admin --> 确认用户是否为管理员
+            '''
+        code_b = "{0:b}".format(code)
+        funcList = [
+            self._enable,       # 0 
+            self._state,        # 1
+            self._blacklist,    # 2
+            self._admin         # 3
+            ]
+        i, r = 0, True
+        if len(code_b) == len(funcList):
+            pass
+        elif len(code_b) < len(funcList):
+            code_b = "0" *(len(funcList) - len(code_b)) + code_b
+        else:
+            raise Exception("code长度错误")
+        while i<len(code_b):
+            if int(code_b[i]):
+                r = funcList[i]() and r
+                i += 1
+            else:
+                i += 1
+                continue
+        return r
+
+    def _enable(self):
+        '''确认模组总开关状态'''
+        return mods.find_one({"name": self.mod_name})["enabled"]
+
+    def _state(self):
+        '''确认模组开关状态'''
+        blacklist = mods.find_one({"name": self.mod_name})["blackList"]
+        if self.event.type == "GroupMessage":
+            return False if self.event.sender.group.id in blacklist['Groups'] else True
+        elif self.event.type == "FriendMessage":
+            return False if self.event.sender.id in blacklist['Friends'] else True
+
+    def _blacklist(self):
+        '''确认用户是否存在于模组黑名单'''
+        print(2)
+        blacklist = mods.find_one({"name": self.mod_name})["blackList"]
+        print(blacklist[str(self.event.sender.group.id)])
+        if str(self.event.sender.group.id) in blacklist:
+            if self.event.sender.id in blacklist[str(self.event.sender.group.id)]:
+                return False
+            else:
+                return True
+        else:
+            return True
+
+    def _admin(self):
+        '''确认用户是否为该群管理员'''
+        return True if self.event.sender.permission != "MEMBER" else False
--- modules/ImageClassify/__init__.py
+++ modules/ImageClassify/__init__.py
@@ -1,23 +1,17 @@
-from array import array
-from ast import List
-from fileinput import close
 import json, re
-import database, criteria
+import database
+from filter import Filter
 from graia.broadcast.interrupt.waiter import Waiter
-from graia.ariadne.message.parser.base import MatchContent, DetectPrefix, ContainKeyword
 from graia.ariadne import get_running
 from graia.ariadne.adapter import Adapter
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage, FriendMessage, MessageEvent
 from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.model import Group, Member
 from graia.broadcast.interrupt import InterruptControl
 from graia.saya import Channel
-from graia.saya.builtins.broadcast.schema import ListenerSchema
 from aip import AipImageClassify
-from graia.broadcast.exceptions import ExecutionStop
 from graia.ariadne.message.commander import Commander, Slot, Arg
-from graia.ariadne.message.commander.saya import CommanderBehaviour, CommandSchema
+from graia.ariadne.message.commander.saya import  CommandSchema
 
 # 获取插件实例，添加插件信息
 channel = Channel.current()
@@ -43,114 +37,90 @@ if mods.find_one({"name": "ImageClassify"}) == None:
         "whiteList": [],
         })
 
-
-# 创建消息处理器，接收群消息中的 .识图 命令
-@channel.use(
-    ListenerSchema(
-    listening_events=[GroupMessage, FriendMessage], 
-    decorators=[
-        MatchContent(".识图"),
-        criteria.check_mod_state("ImageClassify"),
-        criteria.check_mod_blacklist("ImageClassify")]
-))
-async def imageClassify(app: Ariadne, event:MessageEvent , message: MessageChain):
-    print (event)
-    sender = event.sender
-    inc = InterruptControl(app.broadcast) # 创建实例
-    send = app.sendFriendMessage if event.type=="FriendMessage" else app.sendGroupMessage
-    await send(sender, MessageChain.create('请发送图片'))
-    # 交互等待函数
-    @Waiter.create_using_function([GroupMessage, FriendMessage])
-    async def waiter(e: MessageEvent, msg: MessageChain):
-        # 判断新消息的群组和用户是否和上文相同，相同则继续。否则结束。
-        print (e)
-        if sender.id == e.sender.id and event.type == e.type:
-            return msg
-
-    # 处理图片消息，获取图片的url
-    imginfo = await inc.wait(waiter)
-    imgurl = re.findall(r'"url":"(.*?)"', imginfo.asPersistentString())[0]
-    # 通过图片url获取图片二进制编码，并传入识图实例
+async def imageClassify(imgurl):
+    '''传入图片链接，返回识图结果'''
     session = get_running(Adapter).session
     async with session.get(imgurl) as r:
-        imgBase = await r.read()
-        info = client.advancedGeneral(imgBase)['result'][0]
-        responseMsg = f'''图片识别结果:
-        关键词：{info['keyword']}
-        准确度：{info['score']*100}%
-        标  签：{info['root']}'''
-    await send(sender, MessageChain.create(responseMsg))
+        info = client.advancedGeneral(await r.read())['result'][0]
+        responseMsg = '图片识别结果:\n关键词：{}\n准确度：{}%\n标  签：{}'
+        return responseMsg.format(info['keyword'], info['score']*100, info['root'])
 
-# 启用模组
 @channel.use(
         CommandSchema(
-        "[command|命令]",
-        {
-            "open": Arg("[--on|-O|-开启]", bool, False), 
-            "close": Arg("[--off|-C|-关闭]", bool, False),
-            "black": Arg("[--black|-B|-拉黑] {}", str, ""),
-            "white": Arg("[--white|-W|-取消拉黑] {}", str, "")
-        },
-    )
-)
+        ".识图",
+        {"open": Arg("[--on|-O|-开启]", bool, False), 
+        "close": Arg("[--off|-C|-关闭]", bool, False),
+        "black": Arg("[--black|-B|-拉黑] {}", str, ""),
+        "white": Arg("[--white|-W|-取消拉黑] {}", str, "")}))
 async def control(open, close, black, white, app: Ariadne, event:MessageEvent, message: MessageChain):
-    print (open, close, black, white)
-    print (message)
-    if open: # 开启
-        blackList = mods.find_one({"name": "ImageClassify"})['blackList']
-        if event.type=="GroupMessage" and event.sender.permission != "MEMBER":
-            if blackList['Groups'].count(event.sender.group.id):
-                blackList['Groups'].remove(event.sender.group.id)
-                mods.update_one({"name": "ImageClassify"}, {"$set": {"blackList": blackList}})
-                await app.sendGroupMessage(event.sender.group.id, MessageChain.create('已开启识图功能'))
-        elif event.type=="FriendMessage":
+    filter = Filter(mod_name="ImageClassify", event=event).main
+    senderID = event.sender.id
+    blackList = mods.find_one({"name": "ImageClassify"})['blackList']
+    # 处理群消息
+    if event.type=="GroupMessage":
+        groupId = event.sender.group.id
+
+        if open==close==False and black==white=="" and filter(14): # 识图
+            inc = InterruptControl(app.broadcast) # 创建实例
+            await app.sendGroupMessage(groupId, MessageChain.create('请发送图片'))
+            @Waiter.create_using_function([GroupMessage])
+            async def waiter(e: MessageEvent, msg: MessageChain):
+                '''判断新消息的群组和用户是否和上文相同，相同则继续。否则结束。'''
+                if senderID == e.sender.id and groupId == e.sender.group.id:
+                    return msg
+            # 处理图片消息，获取图片的url
+            imginfo = await inc.wait(waiter)
+            imgurl = re.findall(r'"url":"(.*?)"', imginfo.asPersistentString())[0]
+            responseMsg = await imageClassify(imgurl)
+            await app.sendGroupMessage(groupId, MessageChain.create(responseMsg))
+
+        if open and filter(9): # 开启
+            if blackList['Groups'].count(groupId):
+                blackList['Groups'].remove(groupId)
+                await app.sendGroupMessage(groupId, MessageChain.create('已开启识图功能'))
+
+        if close and filter(13): # 关闭
+            if not blackList['Groups'].count(groupId):
+                blackList['Groups'].append(groupId)
+                await app.sendGroupMessage(groupId, MessageChain.create('已关闭识图功能'))
+
+        if black !="" and filter(13): # 拉黑
+            userId = list(map(lambda x: int(x), re.findall(r'(\d+)', black)))
+            blackList[str(groupId)] = blackList[str(groupId)] if str(groupId) in blackList else []
+            blackList[str(groupId)] = list(set(blackList[str(groupId)] +  userId))
+            await app.sendGroupMessage(groupId, MessageChain.create(f'已将{userId}加入黑名单！'))
+
+        if white !="" and filter(13): # 取消拉黑
+            userId = list(map(lambda x: int(x), re.findall(r'(\d+)', white)))
+            blackList[str(groupId)] = blackList[str(groupId)] if str(groupId) in blackList else []
+            blackList[str(groupId)] = list(set(blackList[str(groupId)]) -  set(userId))
+            await app.sendGroupMessage(groupId, MessageChain.create(f'已将{userId}移出黑名单！'))
+
+    # 处理好友消息
+    elif event.type=="FriendMessage":
+        if open==close==False and black==white=="" and filter(12): # 识图
+            inc = InterruptControl(app.broadcast) # 创建实例
+            await app.sendFriendMessage(senderID, MessageChain.create('请发送图片'))
+            @Waiter.create_using_function([FriendMessage])
+            async def waiter(e: MessageEvent, msg: MessageChain):
+                '''判断新消息的群组和用户是否和上文相同，相同则继续。否则结束。'''
+                if senderID == e.sender.id:
+                    return msg
+            # 处理图片消息，获取图片的url
+            imginfo = await inc.wait(waiter)
+            imgurl = re.findall(r'"url":"(.*?)"', imginfo.asPersistentString())[0]
+            responseMsg = await imageClassify(imgurl)
+            await app.sendFriendMessage(senderID, MessageChain.create(responseMsg))
+
+        if open and filter(8): # 开启
             if blackList['Friends'].count(event.sender.id):
                 blackList['Friends'].remove(event.sender.id)
-                mods.update_one({"name": "ImageClassify"}, {"$set": {"blackList": blackList}})
                 await app.sendFriendMessage(event.sender.id, MessageChain.create('已开启识图功能'))
 
-    if close: # 关闭
-        blackList = mods.find_one({"name": "ImageClassify"})['blackList']  
-        if event.type=="GroupMessage" and event.sender.permission != "MEMBER":
-            if not blackList['Groups'].count(event.sender.group.id):
-                blackList['Groups'].append(event.sender.group.id)
-                mods.update_one({"name": "ImageClassify"}, {"$set": {"blackList": blackList}})
-                await app.sendGroupMessage(event.sender.group.id, MessageChain.create('已关闭识图功能'))
-        elif event.type=="FriendMessage":
+        if close and filter(12): # 关闭
             if not blackList['Friends'].count(event.sender.id):
                 blackList['Friends'].append(event.sender.id)
-                mods.update_one({"name": "ImageClassify"}, {"$set": {"blackList": blackList}})
                 await app.sendFriendMessage(event.sender.id, MessageChain.create('已关闭识图功能'))
 
-
-# 将用户加入黑名单
-@channel.use(ListenerSchema(
-    listening_events=[GroupMessage], 
-    decorators=[
-        DetectPrefix(".识图"), 
-        ContainKeyword(keyword="-拉黑"), 
-        criteria.check_mod_blacklist("ImageClassify"),
-        criteria.check_mod_state("ImageClassify")]))
-async def control(app: Ariadne, group: Group, member: Member, message: MessageChain):
-    criteria.check_group_admin(member.permission)
-    userId = list(map(lambda x: int(x), re.findall(r'@(\d+)', message.asDisplay())))
-    blackList = mods.find_one({"name": "ImageClassify"})['blackList']
-    blackList = list(set(blackList +  userId))
+    # 更新数据库
     mods.update_one({"name": "ImageClassify"}, {"$set": {"blackList": blackList}})
-    await app.sendGroupMessage(group, MessageChain.create(f'已将{userId}加入黑名单！'))
-
-# 将用户移出黑名单
-@channel.use(ListenerSchema(
-    listening_events=[GroupMessage], 
-    decorators=[
-        DetectPrefix(".识图"), 
-        ContainKeyword(keyword="-取消拉黑"), 
-        criteria.check_mod_blacklist("ImageClassify"),
-        criteria.check_mod_state("ImageClassify")]))
-async def control(app: Ariadne, group: Group, member: Member, message: MessageChain):
-    criteria.check_group_admin(member.permission)
-    userId = list(map(lambda x: int(x), re.findall(r'@(\d+)', message.asDisplay())))
-    blackList = mods.find_one({"name": "ImageClassify"})['blackList']
-    blackList = list(set(blackList) -  set(userId))
-    mods.update_one({"name": "ImageClassify"}, {"$set": {"blackList": blackList}})
-    await app.sendGroupMessage(group, MessageChain.create(f'已将{userId}移出黑名单！'))
